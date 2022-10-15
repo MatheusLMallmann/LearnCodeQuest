3ce744ed5ca3a534e4e73ad50304db81505e9a65
MatheusLMallmann <matheusl.mallmann@gmail.com>
2022-10-15 14:41:54 -0300

    Atualizando navbar

--- learncodequest/src/components/layout/navbar.js
+++ learncodequest/src/components/layout/navbar.js
@@ -1,3 +1,16 @@
-function navbar(){
-    return <p className = "header-font">logo</p>;
+import './navbar-module.css';
+
+function Navbar(){
+    return (        
+        <div className="header">              
+            <p className = "header-font">logo</p>
+
+            <div className="register-buttons">
+                <p className="header-criar-conta">Criar conta</p>
+                <button className="header-entrar">Entrar</button> 
+            </div>           
+        </div> 
+    );
 }
+
+export default Navbar;
