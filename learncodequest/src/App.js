76277da4613b7681c4e26835f16a32543c3281a2
Leonardo Farias <leooliveiradefarias@gmail.com>
2022-10-15 23:32:50 -0300

    criado paginas e router

--- learncodequest/src/App.js
+++ learncodequest/src/App.js
@@ -1,24 +1,31 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home'
+import Login from './components/pages/Login'
+import Profile from './components/pages/Profile'
+import Register from './components/pages/Register'
+import Navbar from './components/layout/Navbar'
 
-import Navbar from './components/layout/navbar';
+
+
 
 function App() {
   return (
-    <Router >
-    <Navbar />
-    <div className="InitdwialPageHeader">
-      
-      <body className='page-body'>
-      
-        <p>
-          Aprenda de uma forma divertida e ganhe recompensas.
-        </p>
-      
-        
-      </body>
-    </div>
-    </Router>
+      <Router>
+        <Navbar/>
+        <div>
+          <Link to ='/'>Home</Link>
+          <Link to ='/Login'>Login</Link>
+          <Link to ='/Profile'>Profile</Link>
+          <Link to ='/Register'>Register</Link>
+        </div>
+        <Routes>
+          <Route path='/' element= {<Home/>}/>
+          <Route path='/Login' element= {<Login/>}/>
+          <Route path='/Profile' element= {<Profile/>}/>
+          <Route path='/Register' element= {<Register/>}/>
+        </Routes>
+      </Router>
   );
 }
 
--- learncodequest/src/components/layout/navbar.js
+++ learncodequest/src/components/layout/navbar.js
@@ -1,4 +1,4 @@
-import './navbar-module.css';
+import './Navbar-module.css';
 
 function Navbar(){
     return (        
@@ -6,7 +6,7 @@ function Navbar(){
             <p className = "header-font">logo</p>
 
             <div className="register-buttons">
-                <p className="header-criar-conta">Criar conta</p>
+                <button className="header-criar-conta">Criar conta</button>
                 <button className="header-entrar">Entrar</button> 
             </div>           
         </div> 
